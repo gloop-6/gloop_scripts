c3094d1f87d1b694cdb09e06bac3d44a9c113c0f
Sea-Pickle <73187296+Sea-Pickle@users.noreply.github.com>
2023-10-02 12:48:01 -0500

    Update julia_new.py
    removed unecessary multiprocessing, added values to change if the 'c' value of the fractal is generated or set

--- julia_new.py
+++ julia_new.py
@@ -1,19 +1,29 @@
 from PIL import Image
 import math
 import functools
-import multiprocessing
 #by _gloop/gloop#5445
 #=======
-w,h = 1000,1000
+w,h = 500,500
 zoom = (0.8,0.8)
 offset = (0,0)
-c = complex(0.37,0.3)
+
+default_c_value = complex(0.5,0.3)
+use_generated_c_value = True
+c_value_offset = 0.31
+
 hue_offset = 0.1
 trig_offset = -math.pi/4
 max_iterations = 50
-chunk_size = 6
 #=======
-
+def generate_c_value(t):
+    return complex(
+            0.7885*math.cos(t+(5.1*8)*math.pi/180),
+            0.7887*math.sin(t+(5.1)*math.pi/180)
+        )
+if use_generated_c_value:
+    c = generate_c_value(c_value_offset)
+else:
+    c = default_c_value
 w,h = w//2,h//2
 @functools.cache
 def julia(z,c):
@@ -35,34 +45,26 @@ def progress_bar(length,n):
     bar = "#"*amount_complete+("."*(length-(amount_complete)))
     return bar + " "*length
 
-def process_line(y):
-    line = []
-    for x in range(-w,w):
-        z = complex(
-            offset[0] + (x / w)/ zoom[0],
-            offset[1] + (y / h)/ zoom[1]
-            )
-        m = julia(z,c)
-        if m==max_iterations:
-            color = (0,0,0)
-        else:
-            color = color_from_trig((m/max_iterations)+hue_offset,offset=trig_offset)
-        line+=[color]
-    return line
+
 def main():
     im  = Image.new( mode = "RGB", size = (w*2, h*2))
-    lines = []
-    with multiprocessing.Pool() as p:
-        for y,result in enumerate(p.imap(process_line, range(-h,h),chunk_size)):
-            lines += result
-            amount_complete = (y)/(h*2)
-            print(f"{amount_complete*100:.0f}% complete... {progress_bar(20,amount_complete)}",end="\r")
-    im.putdata(lines)
+    data = []
+    for y in range(-h,h):
+        if y%10==0:
+            amt_complete = (y/(h*2))+0.5
+            print(f"{amt_complete*100:.0f}%",progress_bar(20,amt_complete),end="    \r")
+        for x in range(-w,w):
+            z = complex(
+                offset[0] + (x / w)/ zoom[0],
+                offset[1] + (y / h)/ zoom[1]
+                )
+            m = julia(z,c)
+            if m==max_iterations:
+                color = (0,0,0)
+            else:
+                color = color_from_trig((m/max_iterations)+hue_offset,offset=trig_offset)
+            data.append(color)
+    im.putdata(data)
     im.save('julia_set_render.png')    
 if __name__ == "__main__":
     main()
-    
-
-
-
-
