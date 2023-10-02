f8ddcd612a2bae424a3cbeed49a9739183e9c370
Sea-Pickle <73187296+Sea-Pickle@users.noreply.github.com>
2023-10-02 12:55:03 -0500

    Update mandelbrot_new.py
    removed unnecessary multiprocessing, changed the 'start/end' system for zooming and positioning for a dedicated zoom and offset system, also added the ability to set the infill color of the fractal

--- mandelbrot_new.py
+++ mandelbrot_new.py
@@ -1,18 +1,19 @@
 from PIL import Image
 import math
 import functools
-import multiprocessing
 #by _gloop/gloop#5445
 #=======
-w,h = 1000,1000
-chunk_size = 30
-hue_offset = 0.1
-r_start,r_end = -0.7,0.7
-i_start,i_end = 0,1
+w,h = 1024,1024
+offset = -0.4,0
+zoom = 0.8,0.8
 max_iterations = 50
-#=======
 
+hue_offset = 0.1
+trig_offset = -math.pi/4
+infill_color = (0,0,0)
+#=======
 w,h = w//2,h//2
+
 @functools.cache
 def mandelbrot(c):
     z = 0
@@ -21,6 +22,7 @@ def mandelbrot(c):
         z = z*z + c
         n += 1
     return n
+
 @functools.cache
 def color_from_trig(h,offset=(math.pi*(2/3))):
     n = h * math.pi
@@ -33,35 +35,27 @@ def progress_bar(length,n):
     amount_complete = int(n*length)
     bar = "#"*amount_complete+("."*(length-(amount_complete)))
     return bar + " "*length
-offset = -math.pi/4
-def process_line(y):
-    line = []
-    for x in range(-w,w):
-        c = complex(
-            r_start + (x / w) * (r_end - r_start),
-            i_start + (y / h) * (i_end - i_start)
-            )
-        m = mandelbrot(c)
-        if m==max_iterations:
-            color = (0,0,0)
-        else:
-            color = color_from_trig((m/max_iterations)+hue_offset,offset=offset)
-        line+=[color]
-    return line
+
 def main():
     im  = Image.new( mode = "RGB", size = (w*2, h*2))
-    lines = []
-    with multiprocessing.Pool() as p:
-        for y,result in enumerate(p.imap(process_line, range(-h,h),chunk_size)):
-            lines += result
-            amount_complete = (y)/(h*2)
-            print(f"{amount_complete*100:.0f}% complete... {progress_bar(20,amount_complete)}",end="\r")
-    im.putdata(lines)
+    data = []
+    for y in range(-h,h):
+        if y%10==0:
+            t = (y/(h*2))+0.5
+            print(f"{t*100:.0f}%",progress_bar(20,t),end="    \r")
+        for x in range(-w,w):
+            z = complex(
+                offset[0] + (x / w)/ zoom[0],
+                offset[1] + (y / h)/ zoom[1]
+                )
+            m = mandelbrot(z)
+            if m==max_iterations:
+                color = infill_color
+            else:
+                color = color_from_trig((m/max_iterations)+hue_offset,offset=trig_offset)
+            data.append(color)
+    im.putdata(data)
     im.save('mandelbrot_set_render.png')    
+
 if __name__ == "__main__":
     main()
-    
-
-
-
-
